b51d04493c86ab97def7b891fc8a731c053e104f
NuhDemir <115398614+NuhDemir@users.noreply.github.com>
2024-06-16 02:13:59 +0300

    new

---
+++ SortedSetKumeUygulamasi/SortedSetKumeUygulamasi/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSetKumeUygulamasi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedSet Küme İşlemleri
+           // var A = new SortedSet<int>() { 1, 2, 3, 4 };
+           var A = new SortedSet<int>(RandomNumberGenerated(20));
+            var B = new SortedSet<int>() { 1, 2, 5, 6 };
+
+            Console.WriteLine();
+            Console.WriteLine("A Kümesi : ");
+
+            #region
+            foreach (int n in A)
+            {
+                Console.WriteLine($"{n,5}");
+                Console.WriteLine("---");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("B Kümesi : ");
+
+
+            foreach (int n in B)
+            {
+                Console.WriteLine($"{n,5}");
+                Console.WriteLine("---");
+            }
+
+            Console.WriteLine();
+            #endregion
+
+
+            //Union
+            Console.WriteLine("A ve B kümesinin Birleşimi");
+            A.UnionWith(B);
+            foreach (var item in A)
+            {
+                Console.WriteLine($"{item,4}");
+            }
+
+            Console.WriteLine("Toplam Sayısı : {0}", A.Count);
+
+
+            Console.ReadKey();
+
+        }
+        static List<int> RandomNumberGenerated(int n)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for (int i = 0; i < n; i++)
+            
+
+                list.Add(r.Next(0, 1000));
+                return list;
+
+            
+
+        }
+    }
+
+     }
+
+    
+
---
+++ SortedSetUygulamasi/SortedSetUygulamasi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSetUygulamasi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            //Rastgele sayılar üretiyoruz
+            var r = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                numbers.Add(r.Next(0,10));
+                Console.WriteLine($"{numbers[i],-3}\n");
+            }
+
+            Console.WriteLine("-");
+
+            //Listedeki benzersiz elemanları bulmak
+            var uniqueNumber = new SortedSet<int>(numbers);
+            Console.WriteLine();
+            Console.WriteLine("Benzersiz Sayılar Listesi :\n");
+            
+            foreach (int i in uniqueNumber)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
